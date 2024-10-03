94c10e86f7611574a65013ecef3ce920cbc06f53
Isaiah <123019005+Isaia26633@users.noreply.github.com>
2024-10-03 11:37:41 -0400

    it move!

--- pet.js
+++ pet.js
@@ -5,9 +5,39 @@ pet.y = 470;
 pet.width = 200;
 pet.height = 200;
 pet.img = new Image();
-pet.img.src = 'img/sprites/hayden/hayden.png';
+pet.img.src = 'img/sprites/hayden/haydenL.png';
+pet.img.id = '1';
 
+
+//direction pet is facing
 //1 is left 0 is right
-pet.direction = 1;
+direction = 1;
 
-pet.move = false;
+pet.moving = true;
+
+
+function draw() {
+    //draws pet
+    ctx.drawImage(pet.img, pet.x, pet.y, pet.width, pet.height);
+    //moves the pet
+    if (pet.moving) {
+        if (direction == 1) {
+            pet.img.src = 'img/sprites/hayden/haydenL.png';
+            pet.img.id = '1';
+            pet.x--;
+            pet.moving = true;
+        } else {
+            pet.img.src = 'img/sprites/hayden/haydenR.png';
+            pet.img.id = '0';
+            pet.x++;
+            pet.moving = true;
+        }
+    }
+    //bounces pet back if it hits the edge of the canvas and changes direction
+    if (pet.x + pet.width > canvas.width) {
+        console.log('hit');
+        direction = 1;
+    } else if (pet.x < 0) {
+        direction = 0;
+    }
+}
